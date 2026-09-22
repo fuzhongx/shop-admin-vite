7c3761577fbe98a702217bed11a4c04596cc6e48
fuzhongxu <2395414930@qq.com>
2026-09-22 21:01:49 +0800

    退出登录功能完善

--- src/layouts/composables/FHeader.vue
+++ src/layouts/composables/FHeader.vue
@@ -4,25 +4,25 @@
             <el-icon>
                 <ElemeFilled />
             </el-icon>
-            <span class="ml-1"> 旭日商城 </span>
+            <span class="ml-1"> 旭日商城后台管理 </span>
         </span>
 
         <el-icon class="icon-btn">
             <Fold />
         </el-icon>
         <el-icon class="icon-btn">
-            <Refresh  @click="handleRefresh"/>
+            <Refresh @click="handleRefresh" />
         </el-icon>
 
         <div class="right">
             <el-icon class="FullScreen">
-                <FullScreen  @click="toggle" v-if="!isFullscreen"/>
-                 <Aim  @click="toggle" v-else/>
+                <FullScreen @click="toggle" v-if="!isFullscreen" />
+                <Aim @click="toggle" v-else />
             </el-icon>
             <el-dropdown class="dropdown" @command="handleCommand">
                 <span class="flex justify-center items-center text-light-50">
-                    <el-avatar :size="30" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" />
-                    <!-- <el-avatar :size="30" :src="$store.state.user.avatar" /> -->
+                    <!-- <el-avatar :size="30" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" /> -->
+                    <el-avatar :size="30" :src="$store.state.user.avatar" />
 
                     <span class="ml-[5px]"> {{ $store.state.user.username }}</span>
                     <el-icon class="el-icon--right">
@@ -38,24 +38,76 @@
             </el-dropdown>
         </div>
     </div>
+
+    <!-- 修改密码 -->
+    <el-drawer v-model="showDrawer" title="修改密码" close-on-click-modal="false">
+        <el-form ref="FormRef" style="max-width: 600px" :model="ruleForm"  :rules="rules" label-width="auto"
+            class="demo-ruleForm">
+            <el-form-item label="旧密码" prop="oldpassword">
+                <el-input v-model="Form.oldpassword" type="password" autocomplete="off" />
+            </el-form-item>
+            <el-form-item label="新密码" prop="password">
+                <el-input v-model="Form.password" type="password" show-password />
+            </el-form-item>
+            <el-form-item label="确认密码" prop="repassword" type="password" show-password>
+                <el-input v-model="Form.repassword" />
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="handleRePassword">
+                   确认
+                </el-button>
+                <el-button @click="resetSubmit(FormRef)">取消</el-button>
+            </el-form-item>
+        </el-form>
+    </el-drawer>
 </template>
 <script setup>
 import { showModel, toast } from '@/composables/util';
 import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
-import { logout } from '@/api/menager.js'
+import { logout, updatepassword } from '@/api/menager.js'
 import { useFullscreen } from '@vueuse/core'
+import { ref } from "vue"
+import { reactive } from 'vue';
 
 //安装npm i @vueuse/core
-const { 
+const {
     //是否全屏
-    isFullscreen, enter, exit, 
+    isFullscreen, enter, exit,
     //调用方法
     toggle } = useFullscreen()
+
+    const FormRef=ref()
+    const Form=reactive({
+        oldpassword:'',
+        password:'',
+        repassword:''
+    })
+
+    const rules={
+        oldpassword:[
+            {required:true,message:'请输入旧密码',trigger:'blur'}
+        ],
+        password:[
+            {required:true,message:'请输入新密码',trigger:'blur'}
+        ],
+        repassword:[
+            {required:true,message:'请输入确认密码',trigger:'blur'}
+        ],
+    }
+
 
 const router = useRouter()
 const store = useStore()
 
+
+// 修改密码操作
+const showDrawer = ref(false)
+const handleRePassword = () => {
+    updatepassword(Form).then(res => {
+      console.log(res);
+    })
+}
 
 /**
  * 
@@ -67,13 +119,18 @@ const handleCommand = (e) => {
             handleLoyout()
             break;
         case 'rePassword':
-            console.log('退出登录');
+            showDrawer.value = true
             break;
     }
 }
 
+const resetSubmit=(e)=>{
+    console.log(e);
+    showDrawer.value = false
+}
+
 //刷新
-const handleRefresh=()=>location.reload()
+const handleRefresh = () => location.reload()
 
 const handleLoyout = () => {
     showModel('是否退出登录').then(() => {
